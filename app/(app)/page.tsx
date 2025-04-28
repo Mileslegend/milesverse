4c4caa5e5c6b6ee81cae331983f11e8ca1291af2
Mileslegend <mileslegend7@gmail.com>
2025-04-28 11:49:53 +0000

    Started Working on the home page and the PostsList

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,11 +1,32 @@
+import PostsList from "@/components/post/PostsList";
 import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hi Miles</h1>
-      <Button>Click Me</Button>
-    </div>
+    <>
+      {/* Banner Section */}
+      <section className="bg-white border-b">
+        <div className="mx-auto max-w-7xl py-6 px-4">
+          <div className="flex items-center">
+            <div>
+              <h1 className="text-2xl font-bold">Home</h1>
+              <p className="text-sm text-gray-600">
+                Recent Posts from all communities
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Posts Section */}
+      <section className="my-8">
+        <div className="mx-auto max-w-7xl px-4">
+          <div className="flex flex-col gap-4">
+            <PostsList />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
